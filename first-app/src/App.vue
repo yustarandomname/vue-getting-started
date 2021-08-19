cff7702b0f50e27cdde157b7fb93f0e251bd15a5
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-08-19 16:54:36 +0200

    lesson #4

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <h2>{{title}}</h2>
+  <div>
+    <h2>{{title}}</h2>
+    <input type="text" ref="name" >
+    <button @click="handleClick">click me</button>
+
+    <Modal />
+  </div>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue"
 
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'Hello Vue!'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.title = this.$refs.name.value
     }
   }
 }
---
+++ first-app/src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal content</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+
+  .modal {
+    width:400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+</style>
